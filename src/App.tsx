32dfe7700c599d9fa37e8d8646de3cd656c20eaf
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-27 09:38:44 +0200

    example implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
-import employeeIcon from "@ui5/webcomponents-icons/dist/employee.js";
-import { Button } from "@ui5/webcomponents-react";
+import {
+  ComboBox,
+  ComboBoxItem,
+  ComboBoxDomRef,
+} from "@ui5/webcomponents-react";
+
+import { useEffect, useRef } from "react";
+
+// mock
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [key: string]: any;
+    }
+  }
+}
 
 function App() {
+  const cbRef = useRef(null);
+
+  useEffect(() => {
+    const cbElement = cbRef.current as ComboBoxDomRef | null;
+    if (cbElement) {
+      cbElement.addEventListener("change", console.log);
+    }
+    return () => {
+      if (cbElement) {
+        cbElement.removeEventListener("change", console.log);
+      }
+    };
+  }, []);
+
   return (
     <>
-      <Button icon={employeeIcon}>Button</Button>
+      {/*w/o ui5 wcr wrapper*/}
+      <ui5-combobox ref={cbRef} value="ComboBox Entry 1">
+        <ui5-cb-item text="ComboBox Entry 1" />
+        <ui5-cb-item text="ComboBox Entry 2" />
+        <ui5-cb-item text="ComboBox Entry 3" />
+      </ui5-combobox>
+      {/*w/ ui5wcr wrapper*/}
+      <ComboBox onChange={console.log} value="ComboBox Entry 1">
+        <ComboBoxItem text="ComboBox Entry 1" />
+        <ComboBoxItem text="ComboBox Entry 2" />
+        <ComboBoxItem text="ComboBox Entry 3" />
+      </ComboBox>
     </>
   );
 }
