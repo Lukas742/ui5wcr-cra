155143504411ad1a471fb7e9f4fb6e47099c7d4d
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-27 09:10:29 +0200

    init wcr

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import employeeIcon from "@ui5/webcomponents-icons/dist/employee.js";
+import { Button } from "@ui5/webcomponents-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button icon={employeeIcon}>Button</Button>
+    </>
   );
 }
 
