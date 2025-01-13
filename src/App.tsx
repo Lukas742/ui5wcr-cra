b6a16808bbd861c886e9d954419cda65ab850d4a
Lukas Harbarth <lukas.harbarth@sap.com>
2025-01-13 15:24:12 +0100

    isolated example for issue 6800

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { version } from "@ui5/webcomponents-react";
+import { version, Select, Option } from "@ui5/webcomponents-react";
+
+// mock intrinsic element types
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [key: string]: any;
+    }
+  }
+}
 
 function App() {
-  return <>UI5WCR Version: {version}</>;
+  return (
+    <>
+      UI5WCR Version: {version}
+      <br />
+      Select wrapped by UI5WCR wrapper:
+      <br />
+      <Select value="Orange">
+        <Option>Apple</Option>
+        <Option>Orange</Option>
+      </Select>
+      <br />
+      Plain ui5-select:
+      <br />
+      <ui5-select value="Orange">
+        <ui5-option>Apple</ui5-option>
+        <ui5-option>Orange</ui5-option>
+      </ui5-select>
+    </>
+  );
 }
 
 export default App;
